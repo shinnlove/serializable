40f80d45a6453b6a7fdaad49617714428c89999f
chensheng.zcs <chensheng.zcs@alipay.com>
2018-06-03 14:21:29 +0800

    序列化初始化

---
+++ src/main/java/Serializer.java
@@ -0,0 +1,36 @@
+/**
+ * Alipay.com Inc.
+ * Copyright (c) 2004-2018 All Rights Reserved.
+ */
+
+/**
+ * 序列化接口。
+ * 提供两个方法：
+ * 1、把任意对象序列化为字节数组；
+ * 2、给定类型把字节数组中对象反序列化出来。
+ *
+ * @author shinnlove.jinsheng
+ * @version $Id: Serializer.java, v 0.1 2018-06-03 下午2:16 shinnlove.jinsheng Exp $$
+ */
+public interface Serializer {
+
+    /**
+     * 任意对象序列化成字节数组。
+     *
+     * @param obj
+     * @param <T>
+     * @return
+     */
+    <T> byte[] serialize(T obj);
+
+    /**
+     * 字节数组反序列化为给定类型对象。
+     *
+     * @param data
+     * @param clazz
+     * @param <T>
+     * @return
+     */
+    <T> T deserialize(byte[] data, Class<T> clazz);
+
+}
